fe6e42cfa9955fe23a11ce433211ff8d3faaeb82
bachsmile <42792197+bachsmile@users.noreply.github.com>
2020-12-25 10:46:54 +0700

    add file unit

---
+++ Feature/Function/function.py
@@ -0,0 +1,48 @@
+#--------------Tile----------------------------------------#
+__author__ = "QC"
+__title__ ="List Function"
+__desc__="""
+    Tong hop danh sach cac function test Feature
+"""
+#--------------End Tile------------------------------------#
+#--------------Import LIB----------------------------------#
+import json
+from airtest.core.api import *
+from airtest.cli.parser import cli_setup
+from poco.drivers.cocosjs import CocosJsPoco
+from poco.exceptions import PocoTargetTimeout
+#--------------End Import Lib------------------------------#
+#--------------Import FILE---------------------------------#
+
+# from Lavuavi.Config.Api import *
+#link cheat:
+from Lavuavi.Config.Api import *
+#----------------------------------------------------------#
+from Lavuavi.Config.config import *
+from Lavuavi.Report.report import *
+from Lavuavi.Img.img import *
+#--------------End Import FILE-----------------------------#
+#--------------Connect Device------------------------------#
+
+if not cli_setup():
+    auto_setup(__file__)
+    
+#--------------End Connect---------------------------------#
+#--------------Poco----------------------------------------#
+
+poco = CocosJsPoco()
+
+#--------------End Poco------------------------------------#
+# script content
+#--------------bien----------------------------------------#
+
+#-------------end bien-------------------------------------#
+#----------------------------Function-------------------------------------------------#
+#Function general:----------------->
+    #Login
+    #CheckUpdateGold
+#Function DB:---------------------->
+#Function VIP:--------------------->
+#Function WC:---------------------->
+#-------------------------------------------------------------------------------------#
+
